eba3d3f7b738389de88f9de91575ac3ddf2d2778
tsukihi <uncreativename0@gmail.com>
2021-03-16 02:50:39 -0500

    feel the rage debug logging

--- cogs/spotify/spotify.py
+++ cogs/spotify/spotify.py
@@ -15,6 +15,7 @@ class Spotify(commands.Cog):
     """
 
     def __init__(self, bot):
+        self.logger = logging.getLogger('zhenpai.spotify')
         self.bot = bot
         self.guilds = None
         self.guild_id_to_role_dict = {}
@@ -39,7 +40,7 @@ class Spotify(commands.Cog):
                 try: 
                     self.guild_id_to_role_dict[guild.id] = await guild.create_role(name=SPOTIFY_ROLE_NAME, hoist=True)
                 except:
-                    await self.send_debug_to_owner("failed to create role in {}".format(guild))
+                    self.logger.info("failed to create role in {}".format(guild))
 
         # how very ugly... :)
         while not self.bot.is_closed():
@@ -50,20 +51,17 @@ class Spotify(commands.Cog):
                 filtered_members = [m for m in guild.members if m.status != Status.offline and not m.bot]
                 for member in filtered_members:
                     types = [a.type for a in member.activities]
+                    self.logger.info(f"member {member}")
                     if ActivityType.listening in types and ActivityType.playing not in types:
                         try:
                             if spotify_role not in member.roles:
                                 await member.add_roles(spotify_role)
                         except Exception as ex:
-                            self.send_debug_to_owner("*** FAILED to ADD role to member {} \n {} \n {}".format(member, member.activities, ex))
+                            self.logger.info("*** FAILED to ADD role to member {} \n {} \n {}".format(member, member.activities, ex))
                     else:
                         try:
                             if spotify_role in member.roles:
                                 await member.remove_roles(spotify_role)
                         except Exception as ex:
-                            self.send_debug_to_owner("*** FAILED to REMOVE role to member {} \n {} \n {}".format(member, member.activities, ex))
+                            self.logger.info("*** FAILED to REMOVE role to member {} \n {} \n {}".format(member, member.activities, ex))
             await asyncio.sleep(BACKGROUND_TASK_LOOP_SECONDS)
-
-    async def send_debug_to_owner(self, message: str):
-        owner = self.bot.get_user(self.bot.owner_id)
-        await owner.send(message)
