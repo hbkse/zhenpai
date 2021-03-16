f8c8faadb07a49d9c517d4c806dbddb54d562937
George <uncreativename0@gmail.com>
2021-03-15 21:10:46 -0500

    Reduce api calls on spotify cog (#12)

--- cogs/spotify/spotify.py
+++ cogs/spotify/spotify.py
@@ -3,7 +3,7 @@ import logging
 from discord import File
 from discord.ext import commands
 from discord.embeds import Embed
-from discord import ActivityType
+from discord import ActivityType, Status
 
 import asyncio
 
@@ -39,22 +39,31 @@ class Spotify(commands.Cog):
                 try: 
                     self.guild_id_to_role_dict[guild.id] = await guild.create_role(name=SPOTIFY_ROLE_NAME, hoist=True)
                 except:
-                    print("failed to create role in {}".format(guild))
+                    await self.send_debug_to_owner("failed to create role in {}".format(guild))
 
+        # how very ugly... :)
         while not self.bot.is_closed():
             for guild in self.guilds:
                 if guild.id not in self.guild_id_to_role_dict:
                     continue
                 spotify_role = self.guild_id_to_role_dict[guild.id]
-                for member in guild.members:
-                    if any(activity.type == ActivityType.listening for activity in member.activities):
+                filtered_members = [m for m in guild.members if m.status != Status.offline and not m.bot]
+                for member in filtered_members:
+                    types = [a.type for a in member.activities]
+                    if ActivityType.listening in types and ActivityType.playing not in types:
                         try:
-                            await member.add_roles(spotify_role)
-                        except:
-                            await self.bot.get_user(self.bot.owner_id).send("bot failed to add role. guild: {} member: {}".format(guild, member))
+                            if spotify_role not in member.roles:
+                                await member.add_roles(spotify_role)
+                        except Exception as ex:
+                            self.send_debug_to_owner("*** FAILED to ADD role to member {} \n {} \n {}".format(member, member.activities, ex))
                     else:
                         try:
-                            await member.remove_roles(spotify_role)
-                        except:
-                            await self.bot.get_user(self.bot.owner_id).send("bot failed to remove role. guild: {} member: {}".format(guild, member))
+                            if spotify_role in member.roles:
+                                await member.remove_roles(spotify_role)
+                        except Exception as ex:
+                            self.send_debug_to_owner("*** FAILED to REMOVE role to member {} \n {} \n {}".format(member, member.activities, ex))
             await asyncio.sleep(BACKGROUND_TASK_LOOP_SECONDS)
+
+    async def send_debug_to_owner(self, message: str):
+        owner = self.bot.get_user(self.bot.owner_id)
+        await owner.send(message)
