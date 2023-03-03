8a36d7f6b2d64ef156a957d1bd7e2ae930466e2d
George Weng <uncreativename0@gmail.com>
2023-03-02 23:11:57 -0600

    hmm

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ For deployment, handle in github action workflow + secrets.
 if os.environ.get('ENV') != 'CI':
     load_dotenv()
 
-DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
-OWNER_ID = os.environ['OWNER_ID'] or ''
-TWITCAST_CLIENT_ID = os.environ['TWITCAST_CLIENT_ID'] or ''
-TWITCAST_CLIENT_SECRET = os.environ['TWITCAST_CLIENT_SECRET'] or ''
-TWITCAST_ACCESS_TOKEN = os.environ['TWITCAST_ACCESS_TOKEN'] or ''
+DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+OWNER_ID = os.environ.get('OWNER_ID') or ''
+TWITCAST_CLIENT_ID = os.environ.get('TWITCAST_CLIENT_ID') or ''
+TWITCAST_CLIENT_SECRET = os.environ.get('TWITCAST_CLIENT_SECRET') or ''
+TWITCAST_ACCESS_TOKEN = os.environ.get('TWITCAST_ACCESS_TOKEN') or ''
 
 COMMIT_HASH = os.environ.get('COMMIT_HASH') or os.environ.get('RAILWAY_GIT_COMMIT_SHA') or "local"
 
