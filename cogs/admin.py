01bc57ff4b3aec48e7b4fc97c82c26bcfa58cb38
George Weng <uncreativename0@gmail.com>
2023-03-21 23:04:05 -0500

    should hide owner commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -14,10 +14,8 @@ class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    async def cog_check(self, ctx: commands.Context) -> bool:
-        return await self.bot.is_owner(ctx.author)
-
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def debug(self, ctx: commands.Context):
         message = [
             f'Logged in as: {self.bot.user}',
@@ -29,11 +27,13 @@ class Admin(commands.Cog):
         await ctx.send("\n".join(message))
 
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def servers(self, ctx: commands.Context):
         servers = "\n".join([f"{guild.name} ({guild.id})" for guild in self.bot.guilds])
         await ctx.send(servers)
 
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def load_cog(self, ctx: commands.Context, ext: str):
         if ext in self.bot.extensions:
             await self.bot.reload_extension(ext)
@@ -43,18 +43,21 @@ class Admin(commands.Cog):
         await ctx.send(f"{ext} successfully loaded.")
 
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def unload_cog(self, ctx: commands.Context, ext: str):
         await self.bot.unload_extension(ext)
         log.info('Unloaded extension: %s', ext)
         await ctx.send(f"{ext} successfully unloaded.")
     
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def change_status(self, ctx: commands.Context, message: str):
         game = discord.Game(name=message)
         await self.bot.change_presence(activity=game)
         await ctx.send("Status successfully changed.")
 
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def logs(self, ctx: commands.Context, lines: Optional[int]):
         num_lines = str(lines) if lines else '20'
 
@@ -67,7 +70,8 @@ class Admin(commands.Cog):
             await ctx.send(f'Error retrieving logs: {e.output.decode("utf-8")}')
 
 
-    @commands.group(invoke_without_command=True)
+    @commands.group(invoke_without_command=True, hidden=True)
+    @commands.is_owner()
     @commands.guild_only()
     async def sync(self, ctx: commands.Context, guild_id: Optional[int], copy: bool = False) -> None:
         """Syncs the slash commands with the given guild"""
@@ -84,6 +88,7 @@ class Admin(commands.Cog):
         await ctx.send(f'Successfully synced {len(commands)} commands')
 
     @sync.command(name='global')
+    @commands.is_owner()
     async def sync_global(self, ctx: commands.Context):
         """Syncs the commands globally"""
 
