3ebca1a91c7829526e6a077dc20f2f1ed817e905
George Weng <uncreativename0@gmail.com>
2023-03-22 01:54:59 -0500

    add syncing

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import logging
 import config
 from aiohttp import ClientSession
 
+from typing import Optional
+
 log: logging.Logger = logging.getLogger(__name__)
 
 extensions = [
@@ -23,17 +25,22 @@ def setup_intents():
     return intents
 
 class Zhenpai(commands.Bot):
-    def __init__(self, http_client: ClientSession):
+    def __init__(self, 
+        http_client: ClientSession, 
+        testing_guild_id: Optional[int] = None
+    ):
         super().__init__(
             command_prefix=config.COMMAND_PREFIX, 
             owner_id=config.OWNER_ID, 
             intents=setup_intents()
         )
         self.http_client = http_client
+        self.testing_guild_id = testing_guild_id or config.TESTING_GUILD_ID
 
     async def on_ready(self):
         log.info('Logged in as: %s', self.user)
         log.info('Discord.py version: %s', discord.__version__)
+        log.info('Commit hash: %s', config.COMMIT_HASH)
 
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
         if isinstance(error, commands.CommandNotFound):
@@ -62,8 +69,21 @@ class Zhenpai(commands.Bot):
             return
         await self.process_commands(message)
 
-    async def _run(self):
+    async def setup_hook(self) -> None:
         for ext in extensions:
             await self.load_extension(ext)
             log.info('Loaded extension: %s', ext)
+        
+        # Sync app_commands for testing guild
+        if self.testing_guild_id:
+            log.info(f'Syncing global commands to {self.testing_guild_id}')
+            guild = discord.Object(self.testing_guild_id)
+            self.tree.copy_global_to(guild=guild)
+            await self.tree.sync(guild=guild)
+            log.info(f'Finished syncing global commands')
+
+        # This would also be a good place to connect to our database and
+        # load anything that should be in memory prior to handling events.
+
+    async def _run(self):
         await self.start(config.DISCORD_BOT_TOKEN)
--- config.py
+++ config.py
@@ -9,10 +9,13 @@ load_dotenv()
 
 DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 OWNER_ID: int = int(os.environ.get('OWNER_ID'))
+TESTING_GUILD_ID: int = int(os.getenv('TESTING_GUILD_ID'))
+
+COMMIT_HASH = os.environ.get('COMMIT_HASH') or os.environ.get('RAILWAY_GIT_COMMIT_SHA') or "local"
+
+# Twitcasting cog
 TWITCAST_CLIENT_ID = os.environ.get('TWITCAST_CLIENT_ID') or ''
 TWITCAST_CLIENT_SECRET = os.environ.get('TWITCAST_CLIENT_SECRET') or ''
 TWITCAST_ACCESS_TOKEN = os.environ.get('TWITCAST_ACCESS_TOKEN') or ''
-
-COMMIT_HASH = os.environ.get('COMMIT_HASH') or os.environ.get('RAILWAY_GIT_COMMIT_SHA') or "local"
 
 COMMAND_PREFIX = "!"
