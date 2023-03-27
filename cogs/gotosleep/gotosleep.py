966d5113e30a32388da7234f9efbe34363883e27
George Weng <uncreativename0@gmail.com>
2023-03-27 01:14:59 -0500

    bump

--- cogs/gotosleep/gotosleep.py
+++ cogs/gotosleep/gotosleep.py
@@ -14,6 +14,8 @@ log: logging.Logger = logging.getLogger(__name__)
 DAY_OPTIONS = ['All', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 GOTOSLEEP_ROLE_NAME = 'zhenpai-gotosleep'
 POLLING_INTERVAL = 1
+# THIS IS GUNNA BREAK BECAUSE OF DAYLIGHT SAVINGS TIME RIGHT? LOL, oh well one day I'll fix it
+CENTRAL_TIMEZONE_TIMEDELTA = datetime.timedelta(hours=-5) # UTC-5
 
 class GoToSleep(commands.Cog):
     """ For people who can't control themselves and sleep on time. """
@@ -33,17 +35,18 @@ class GoToSleep(commands.Cog):
 
         return discord.utils.get(guild.roles, name=GOTOSLEEP_ROLE_NAME) 
     
-    def _convert_time_to_utc(self, time_str: str, utcoff: datetime.timedelta) -> datetime.time:
+    def _read_time_str(self, time_str: str, utcoff: datetime.timedelta) -> datetime.time:
         """ Convert the time from a string to utc naive datetime. """
 
-        # regex for "0:00 to 23:59"
-        time_regex = re.compile(r'^([0-9]|[01]\d|2[0-3]):([0-5]\d)$')
+        time_regex = re.compile(r'^([0-9]|[01]\d|2[0-3]):([0-5]\d)$') # regex for "0:00 to 23:59"
         hour, minute = map(int, time_regex.match(time_str).groups())
-        time = datetime.time(hour, minute)
+        return self._apply_time_offset(datetime.time(hour, minute), utcoff)
+    
+    def _apply_time_offset(self, time: datetime.time, utcoff: datetime.timedelta) -> datetime.time:
+        """ Apply utc offset to a time. """
 
         # convert to datetime, add utc offset, then convert back to time
-        today = datetime.date.today()
-        return (datetime.datetime.combine(today, time) + utcoff).time()
+        return (datetime.datetime.combine(datetime.date.today(), time) + utcoff).time()
     
     def _is_time_inbetween(self, time: datetime.time, start: datetime.time, end: datetime.time) -> bool:
         """ Check if a time is inbetween two other times, including wrapping around. """
@@ -72,8 +75,9 @@ class GoToSleep(commands.Cog):
 
         await self._setup_new_user_if_needed(interaction)
         try:
-            converted_start_time = self._convert_time(sleep_time, interaction.created_at.utcoffset())
-            converted_end_time = self._convert_time(wake_time, interaction.created_at.utcoffset())
+            # convert to utc+0
+            converted_start_time = self._read_time_str(sleep_time, -CENTRAL_TIMEZONE_TIMEDELTA)
+            converted_end_time = self._read_time_str(wake_time, -CENTRAL_TIMEZONE_TIMEDELTA)
         except Exception as e:
             log.error(f'{e}')
             log.error(f'Unable to read the times provided. {sleep_time} {wake_time}')
@@ -88,7 +92,7 @@ class GoToSleep(commands.Cog):
 
     @app_commands.command()
     async def snooze(self, interaction: discord.Interaction):
-        """ Deactivate gotosleep until you turn it back on. """
+        """ Activate/deactivate gotosleep. """
 
         await self._setup_new_user_if_needed(interaction)
         record = await self.db.get_by_user_id(interaction.user.id, interaction.guild.id)
@@ -107,13 +111,23 @@ class GoToSleep(commands.Cog):
 
         await self._setup_new_user_if_needed(interaction)
         record = await self.db.get_by_user_id(interaction.user.id, interaction.guild.id)
+        if not record:
+            # don't think this should ever happen, since we set up a new user if needed
+            await interaction.response.send_message('You have not set any times yet. Use /sleep to set times.', ephemeral=True)
+            return
+        
         title = f"{interaction.user.display_name}\'s gotosleep times"
         embed = discord.Embed(title=title, description='The times you have set for sleeping and waking up. Times are in UTC (for now).', color=discord.Color.dark_teal())
         active_text = bool(record['active']) and 'gotosleep is currently on.' or 'gotosleep is currently off.'
         active_text += ' Use /snooze to turn it on or off.'
         embed.set_footer(text=active_text)
         for day in DAY_OPTIONS[1:]:
-            embed.add_field(name=day, value=f'{record[day.lower() + "_start_time"]} - {record[day.lower() + "_end_time"]}')
+            start_time = record[day.lower() + "_start_time"]
+            end_time = record[day.lower() + "_end_time"]
+            # convert from utc+0 to central time
+            converted_start_time = self._apply_time_offset(start_time, CENTRAL_TIMEZONE_TIMEDELTA)
+            converted_end_time = self._apply_time_offset(end_time, CENTRAL_TIMEZONE_TIMEDELTA)
+            embed.add_field(name=day, value=f'{converted_start_time} - {converted_end_time}')
         await interaction.response.send_message(embed=embed)
 
     @tasks.loop(minutes=POLLING_INTERVAL)
@@ -138,16 +152,19 @@ class GoToSleep(commands.Cog):
 
                 # if any of these are true, let's just remove the role to be safe
                 if not active or start is None or end is None:
-                    log.info(f'Removing role from {member} in {guild}.')
-                    await member.remove_roles(role)
+                    if role in member.roles:
+                        log.info(f'Removing role from {member} in {guild}.')
+                        await member.remove_roles(role)
                     continue
 
                 if self._is_time_inbetween(datetime.datetime.now().time(), start, end):
-                    log.info(f'Adding role to {member} in {guild}.')
-                    await member.add_roles(role)
+                    if role not in member.roles:
+                        log.info(f'Adding role to {member} in {guild}.')
+                        await member.add_roles(role)
                 else:
-                    log.info(f'Removing role from {member} in {guild}.')
-                    await member.remove_roles(role)
+                    if role in member.roles:
+                        log.info(f'Removing role from {member} in {guild}.')
+                        await member.remove_roles(role)
         except Exception as e:
             log.error(f'Error in gotosleep update_roles: {e}')
             log.info(record)
