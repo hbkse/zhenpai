e6f9ba223cc17dccade3d27700ae6ef0c9230bd3
George Weng <uncreativename0@gmail.com>
2024-05-20 11:17:17 +0900

    update Saturday time

--- cogs/saturday/saturday.py
+++ cogs/saturday/saturday.py
@@ -6,7 +6,7 @@ from discord.ext import commands, tasks
 
 log: logging.Logger = logging.getLogger(__name__)
 
-NINE_AM_CENTRAL = datetime.time(hour=9, minute=0, tzinfo=datetime.timezone(datetime.timedelta(hours=-6)))
+SEVEN_AM_CENTRAL = datetime.time(hour=7, minute=0, tzinfo=datetime.timezone(datetime.timedelta(hours=-6)))
 SATURDAY_WEEKDAY = 5
 IMAGE_NAME = './cogs/saturday/Saturday.jpg'
 USER_ID = 118956121913425920
@@ -23,10 +23,10 @@ class Saturday(commands.Cog):
         self.my_task.cancel()
 
     # checks everyday at 9am Central Time
-    @tasks.loop(time=NINE_AM_CENTRAL)
+    @tasks.loop(time=SEVEN_AM_CENTRAL)
     async def my_task(self):
         # check if today is Saturday
-        if datetime.date.weekday() == SATURDAY_WEEKDAY:
+        if datetime.date.today().weekday() == SATURDAY_WEEKDAY:
             dm_channel = self.bot.get_user(USER_ID)
             await dm_channel.send(file=discord.File(IMAGE_NAME))
 
